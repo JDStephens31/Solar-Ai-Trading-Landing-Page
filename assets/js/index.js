0e012e12b67e8949336179c497d877397c2f929a
Jonathan Stephens <83253662+JDStephens31@users.noreply.github.com>
2024-06-24 13:20:41 -0400

    Hero Mobile Optimization

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,8 @@ document.addEventListener('scroll', function() {
     document.querySelector('.hero-title').classList.remove('fade-in-up')
     let scrollTop = window.scrollY;
     let parallaxValue = scrollTop * 0.1; // Adjust this value for desired parallax effect
-
-    // Apply parallax effect to Mars image
-    document.querySelector('.hero-title').style.transform = `translateY(${parallaxValue}px)`;
-
+    
+    if(window.innerWidth > 1000){
+        document.querySelector('.hero-title').style.transform = `translateY(${parallaxValue}px)`;
+    }
 });
